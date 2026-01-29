e2a23f052eb3adab3482914868162be8d43e04e0
Hanno <hannovdw1998@gmail.com>
2026-01-29 08:02:09 +0200

    Added DTOs

---
+++ src/main/java/com/pollinate/challenge/controller/dto/ProductResponse.java
@@ -0,0 +1,17 @@
+package com.pollinate.challenge.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+  private Long id;
+  private String name;
+  private BigDecimal price;
+}
